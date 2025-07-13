f02e4628fb8f35d554f495c86904f3649c5983d8
Amira Abdurahman <amiraabdurahman8@gmail.com>
2025-07-13 19:23:03 +0300

    execution file added

---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+class ExecuteQuery:
+    def __init__(self, db_name, query, params=None):
+        self.db_name = db_name
+        self.query = query
+        self.params = params or ()
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        self.cursor.execute(self.query, self.params)
+        return self.cursor.fetchall()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.close()
+
+# Usage
+with ExecuteQuery('users.db', "SELECT * FROM users WHERE age > ?", (25,)) as results:
+    for row in results:
+        print(row)
