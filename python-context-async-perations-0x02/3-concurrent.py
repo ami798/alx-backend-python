8e05826814a891e586fac57d5f9d071f29a5a2ca
Amira Abdurahman <amiraabdurahman8@gmail.com>
2025-07-13 19:27:25 +0300

    checker required line added

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -5,13 +5,14 @@ async def async_fetch_users():
     async with aiosqlite.connect("users.db") as db:
         async with db.execute("SELECT * FROM users") as cursor:
             rows = await cursor.fetchall()
-            return rows  # ✅ required by checker
+            return rows  
+            
 
 async def async_fetch_older_users():
     async with aiosqlite.connect("users.db") as db:
         async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
             rows = await cursor.fetchall()
-            return rows  # ✅ required by checker
+            return rows  
 
 async def fetch_concurrently():
     users, older_users = await asyncio.gather(
@@ -27,6 +28,6 @@ async def fetch_concurrently():
     for user in older_users:
         print(user)
 
-# ✅ Run it
+# Run 
 if __name__ == "__main__":
     asyncio.run(fetch_concurrently())
