d1f3efd4cb50f397b6e9e025284dc87f147b50f8
Amira Abdurahman <amiraabdurahman8@gmail.com>
2025-07-27 19:05:22 +0300

    added the urls files

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,8 +1,18 @@
+# messaging_app/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # JWT authentication endpoints
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Your app routes
     path('api/', include('chats.urls')),
-    path('api-auth/', include('rest_framework.urls')),  # Required by checker
+
+    # DRF's built-in login view (used for browsable API)
+    path('api-auth/', include('rest_framework.urls')),
 ]
