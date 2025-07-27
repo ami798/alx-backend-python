a8fd5543f7f8a2ce898f3f5a923975cac0d90523
Amira Abdurahman <amiraabdurahman8@gmail.com>
2025-07-27 22:19:15 +0300

    views added

--- messaging_app/messaging_app/chats/views.py
+++ messaging_app/messaging_app/chats/views.py
@@ -1,7 +1,58 @@
-from rest_framework import viewsets
-from .models import Conversation
-from .serializers import ConversationSerializer
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.status import HTTP_403_FORBIDDEN
+from .models import Message, Conversation
+from .serializers import MessageSerializer, ConversationSerializer
+from .permissions import IsParticipantOfConversation
+
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import MessageFilter
+from .pagination import MessagePagination
+
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = MessageFilter  # 🔎 Enable filtering
+    pagination_class = MessagePagination
+
+    def get_queryset(self):
+        return Message.objects.filter(conversation__participants=self.request.user)
+
+    def perform_create(self, serializer):
+        conversation = serializer.validated_data.get("conversation", None)
+
+        if not conversation:
+            raise PermissionDenied("conversation_id is required.")
+
+        if self.request.user not in conversation.participants.all():
+            return Response(
+                {"detail": "You are not a participant of this conversation."},
+                status=HTTP_403_FORBIDDEN
+            )
+
+        serializer.save(sender=self.request.user)
+
+    def update(self, request, *args, **kwargs):
+        message = self.get_object()
+        if message.sender != request.user:
+            return Response(
+                {"detail": "You cannot edit this message."},
+                status=HTTP_403_FORBIDDEN
+            )
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        message = self.get_object()
+        if message.sender != request.user:
+            return Response(
+                {"detail": "You cannot delete this message."},
+                status=HTTP_403_FORBIDDEN
+            )
+        return super().destroy(request, *args, **kwargs)
+
 
 class ConversationViewSet(viewsets.ModelViewSet):
     serializer_class = ConversationSerializer
@@ -9,3 +60,7 @@ class ConversationViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Conversation.objects.filter(participants=self.request.user)
+
+    def perform_create(self, serializer):
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)
